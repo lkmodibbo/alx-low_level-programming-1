2eaf9b738902e8bdb95e09d7f7db7bf48ed6c6d3
lkmodibbo <lukmansaidmodibbo.com>
2022-09-08 09:46:03 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
-#include<stdio.h>
-int main(void) 
-{
-put ("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+*Description: using the main function
+*this program prints "programming is like building a multilingual puzzle"
+* Return: 0
+*/
+int main(void)
+{ 
+	prints("\"programming is like building a multilingual puzzle');
 	return (0);
 }
