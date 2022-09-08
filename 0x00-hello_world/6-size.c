5c994896b9aa19ed65aff3fbb4a2827295e32823
lkmodibbo <lukmansaidmodibbo.com>
2022-09-08 15:52:33 -0700

    size-
    git push
    git commit -m size-

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - print the string in the put function
+ *
+ *Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long 11;
+	long long 111;
+	float f;
+
+	printf("size of a char: %ld byte(s)\n", sizeof (c));
+	printf("size of an int: %ld byte(s)\n", sizeof(i));
+	printf("size of a long int:%ld byte(s)\n", sizeof(11);
+	printf("size of a long long int:%ld byte(s)\n".sizeof(111));
+	printf("size of a float: %ld byte(s)\n", sizeof(f));
+	return(0);
+}
