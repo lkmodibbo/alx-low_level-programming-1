dc66e409598f46e476f82e4d717b2c70694949e7
lkmodibbo <lukmansaidmodibbo.com>
2022-09-09 10:01:26 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+char lower_case;
+char upper_case;
+for (lower_case = 'a'; lowe_case <= 'z'; lower_case++)
+{
+putchar(lower_case);
+}
+for (upper_case = 'A'; upeer_case <= 'Z' upper_case++)
+{
+putchar(upper_case);
+}
+putchar('\n');
+return (0);
+}
