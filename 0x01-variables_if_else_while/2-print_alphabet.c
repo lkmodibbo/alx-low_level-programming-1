e35c6bed291952c08702a0a543defcc5cc22a7a1
lkmodibbo <lukmansaidmodibbo.com>
2022-09-09 09:41:22 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet =  'a'; alphabet <= 'z'; alphabet++)
+{
+put char (alphabet);
+}
+put char('\n');
+return (0);
+}
