af817f626edea7900debd3c1a8ed729f60cd297d
lkmodibbo <lukmansaidmodibbo.com>
2022-09-08 17:02:37 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - prints a text
  *
  * Description: using the main function
- * this program prints "and that piece of art is useful\ - Dora Korpar,"
+ * this program prints "and that piece of art is useful,"
  * Return: 1
  */
 int main(void)
