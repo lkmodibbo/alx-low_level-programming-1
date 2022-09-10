dc0f98d0e291e50f03f14adc0831148921f50932
lkmodibbo <lukmansaidmodibbo.com>
2022-09-09 17:23:28 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,12 +5,12 @@
  * Description: prints two digits combination
  * Return: Always 0 (success)
  */
-int main(void) 
+int main(void)
 {
 int c, i;
-for (c = '0'; c <= '9'; c++)    
+for (c = '0'; c <= '9'; c++)
 {
-for (i = '0'; i <= '9'; i++)   
+for (i = '0'; i <= '9'; i++)
 {
 if (c < i)
 {
